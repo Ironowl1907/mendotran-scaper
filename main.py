2b50af6daef6d7d8945bb4893404a6ff4b2fc4dd
Ironowl1907 <facuguialtana7@gmail.com>
2026-03-13 22:14:16 +0000

    Updating URL, params and headers and now working

--- main.py
+++ main.py
@@ -1,37 +1,38 @@
 # Mendotran requester
 import requests
-import time
 
-url = "https://owa.visionblo.com/api/mendoza/stops"
+url = "https://mendotran.oba.visionblo.com/oba_api/api/where/stops-for-location.json"
+
 headers = {
     "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:148.0) Gecko/20100101 Firefox/148.0",
     "Accept": "*/*",
-    "Referer": "https://owa.visionblo.com/web/mendoza/",
-    "Content-Type": "application/json",
-    "Origin": "https://owa.visionblo.com"
+    "Accept-Language": "en-US,en;q=0.9",
+    "Connection": "keep-alive",
+    "Referer": "https://mendotran.oba.visionblo.com/",
+    "Sec-Fetch-Dest": "empty",
+    "Sec-Fetch-Mode": "cors",
+    "Sec-Fetch-Site": "same-origin",
 }
 
 # HACK: lat, lon, latSpan and lonSpan should change dynamically
-mendotran_request_stops_header = {
-    "platform":"web",
+mendotran_request_stops_params = {
+    "platform": "web",
     "v": "",
-    "lat":"-32.09676406635843",
-    "lon":"-69.62585449218751",
-    "latSpan":"4.8667193939577835",
-    "lonSpan":"9.788818359375",
-    "version":"1.0",
+    "lat": "-32.74167391740799",
+    "lon": "-68.70059967041017",
+    "latSpan": "0.3742517721891758",
+    "lonSpan": "1.2805938720703125",
+    "version": "1.0",
 }
 
+def main():
+    mendotran_request_stops(mendotran_request_stops_params)
 
-def main() :
-    mendotran_request_stops(mendotran_request_stops_header)
+def mendotran_request_stops(params):
+    r = requests.get(url, params=params, headers=headers)
+    print(r.status_code)
+    print(r.json())
+    return r
 
-def mendotran_request_stops(payload):
-    r = requests.get(url, data = payload, headers = headers)
-    print(r)
-    return
-
-
-
-if (__name__ == "__main__"):
+if __name__ == "__main__":
     main()
